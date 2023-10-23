a2abe12fb4001b851cb92d35e177bac3f0ce41c7
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-23 22:20:00 +0300

    Slash Command poll added (23.10.2023)

--- 01.CreatAndSet/Presentation/CreatAndSet.Presentation/SlashCommands/FunSL.cs
+++ 01.CreatAndSet/Presentation/CreatAndSet.Presentation/SlashCommands/FunSL.cs
@@ -1,5 +1,8 @@
-﻿using DSharpPlus.Entities;
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
 using DSharpPlus.SlashCommands;
+using Presentation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +15,97 @@ namespace CreatAndSet.Presentation.SlashCommands
 
 	{
 		[SlashCommand("Test", "This is first Slash Command")]
-		public async Task TestSlashCommand(InteractionContext ctx)
+		public async Task TestSlashCommand(InteractionContext ctx, [Choice("Pre-Defined Text","ıwghoıgahaghıwhowh" )] [Option("String", "Type in anything you want ") ] string text )
 
 		{
+			await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Starting Slash Command..."));
+
 			var embedMessage = new DiscordEmbedBuilder()
 			{
-				Title = "Test"
+				Title = text
 			};
 			await ctx.Channel.SendMessageAsync(embed: embedMessage);
+		}
+
+		[SlashCommand("poll", "Create your own poll")]
+		public async Task PollCommand(InteractionContext ctx, [Option("question", "The main poll subject/question")] string Question,
+															[Option("timelimit", "The time set on this poll")] long TimeLimit,
+															[Option("option1", "Option 1")] string Option1,
+															[Option("option2", "Option 1")] string Option2,
+															[Option("option3", "Option 1")] string Option3,
+															[Option("option4", "Option 1")] string Option4)
+		{
+			await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+																				.WithContent("..."));
+
+			var interactvity = Program.Client.GetInteractivity(); 
+			TimeSpan timer = TimeSpan.FromSeconds(TimeLimit); 
+
+			DiscordEmoji[] optionEmojis = { DiscordEmoji.FromName(Program.Client, ":one:", false),
+											DiscordEmoji.FromName(Program.Client, ":two:", false),
+											DiscordEmoji.FromName(Program.Client, ":three:", false),
+											DiscordEmoji.FromName(Program.Client, ":four:", false) }; 
+
+			string optionsString = optionEmojis[0] + " | " + Option1 + "\n" +
+								   optionEmojis[1] + " | " + Option2 + "\n" +
+								   optionEmojis[2] + " | " + Option3 + "\n" +
+								   optionEmojis[3] + " | " + Option4; 
+
+			var pollMessage = new DiscordMessageBuilder()
+				.AddEmbed(new DiscordEmbedBuilder()
+					.WithColor(DiscordColor.Azure)
+					.WithTitle(string.Join(" ", Question))
+					.WithDescription(optionsString)); 
+
+			var putReactOn = await ctx.Channel.SendMessageAsync(pollMessage); 
+
+			foreach (var emoji in optionEmojis)
+			{
+				await putReactOn.CreateReactionAsync(emoji); 
+			}
+
+			var result = await interactvity.CollectReactionsAsync(putReactOn, timer); 
+
+			int count1 = 0; 
+			int count2 = 0;
+			int count3 = 0;
+			int count4 = 0;
+
+			foreach (var emoji in result) 
+			{
+				if (emoji.Emoji == optionEmojis[0])
+				{
+					count1++;
+				}
+				if (emoji.Emoji == optionEmojis[1])
+				{
+					count2++;
+				}
+				if (emoji.Emoji == optionEmojis[2])
+				{
+					count3++;
+				}
+				if (emoji.Emoji == optionEmojis[3])
+				{
+					count4++;
+				}
+			}
+
+			int totalVotes = count1 + count2 + count3 + count4;
+
+			string resultsString = optionEmojis[0] + ": " + count1 + " Votes \n" +
+								   optionEmojis[1] + ": " + count2 + " Votes \n" +
+								   optionEmojis[2] + ": " + count3 + " Votes \n" +
+								   optionEmojis[3] + ": " + count4 + " Votes \n\n" +
+								   "The total number of votes is " + totalVotes; 
+
+			var resultsMessage = new DiscordMessageBuilder()
+				.AddEmbed(new DiscordEmbedBuilder()
+					.WithColor(DiscordColor.Green)
+					.WithTitle("Results of Poll")
+					.WithDescription(resultsString));
+
+			await ctx.Channel.SendMessageAsync(resultsMessage);          
 		}
 
 	}
